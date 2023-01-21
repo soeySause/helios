281451bb143f2bd84db5e2b2057cec878aba0c87
Soey <64677181+soeySause@users.noreply.github.com>
2023-01-21 13:19:04 -0600

    user class

--- include/event.hpp
+++ include/event.hpp
@@ -21,26 +21,6 @@ namespace helios {
     class invite;
     class inviteMetadata;
     class guildOptions;
-    class user {
-    public:
-        bool operator==(const user& userToCompare) const;
-        explicit operator bool() const;
-        std::optional<long> id;
-        std::optional<std::string> username;
-        std::optional<std::string> discriminator;
-        std::optional<std::string> avatar;
-        std::optional<bool> bot;
-        std::optional<bool> system;
-        std::optional<bool> mfaEnabled;
-        std::optional<std::string> banner;
-        std::optional<int> accentColor;
-        std::optional<std::string> locale;
-        std::optional<bool> verified;
-        std::optional<std::string> email;
-        std::optional<int> flags;
-        std::optional<int> premiumType;
-        std::optional<int> publicFlags;
-    };
 
     class roleTags {
     public:
--- src/discordClasses/user/user.cpp
+++ src/discordClasses/user/user.cpp
@@ -1,5 +1,30 @@
-//
-// Created by Arish Ali on 1/20/23.
-//
-
 #include "discordClassses/user.hpp"
+
+helios::user helios::user::getUserData(const nlohmann::json& jsonData) {
+    user userData;
+    if(jsonData.contains("id")) {userData.id = std::stol(jsonData["id"].get<std::string>()); }
+    if(jsonData.contains("username")) {userData.username = jsonData["username"]; }
+    if(jsonData.contains("discriminator")) {userData.discriminator = jsonData["discriminator"]; }
+    if(jsonData.contains("avatar") && !jsonData["avatar"].is_null()) {userData.avatar = jsonData["avatar"]; }
+    if(jsonData.contains("bot")) {userData.bot = jsonData["bot"]; }
+    if(jsonData.contains("system")) {userData.system = jsonData["system"]; }
+    if(jsonData.contains("mfa_enabled")) {userData.mfaEnabled = jsonData["mfa_enabled"]; }
+    if(jsonData.contains("banner") && !jsonData["banner"].is_null()) {userData.banner = jsonData["banner"]; }
+    if(jsonData.contains("accent_color") && !jsonData["accent_color"].is_null()) {userData.accentColor = jsonData["accent_color"]; }
+    if(jsonData.contains("locale")) {userData.locale = jsonData["locale"]; }
+    if(jsonData.contains("verified")) {userData.verified = jsonData["verified"]; }
+    if(jsonData.contains("email") && !jsonData["email"].is_null()) {userData.email = jsonData["email"]; }
+    if(jsonData.contains("flags")) {userData.flags = jsonData["flags"]; }
+    if(jsonData.contains("premium_type")) {userData.premiumType = jsonData["premium_type"]; }
+    if(jsonData.contains("avatar")) {userData.avatar = jsonData["avatar"]; }
+    if(jsonData.contains("public_flags")) {userData.publicFlags = jsonData["public_flags"]; }
+    return userData;
+}
+
+bool helios::user::operator==(const user &userToCompare) const {
+    return this->id == userToCompare.id;
+}
+
+helios::user::operator bool() const {
+    return this->id.has_value();
+}
